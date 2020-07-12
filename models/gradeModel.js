b230e9a2253d8c90c04edf5deb4f43e9ada106f9
Valdinei Reis <valdineimtz@gmail.com>
2020-07-12 01:40:59 -0300

    Tratamento do atributo _id

--- models/gradeModel.js
+++ models/gradeModel.js
@@ -20,6 +20,14 @@ export default (mongoose) => {
         lastModified: Date,
     });
 
+    schema.method('toJSON', function () {
+        const { __v, _id, ...object } = this.toObject();
+
+        object.id = _id;
+
+        return object;
+    });
+
     const Grade = mongoose.model('grade', schema, 'grades');
 
     return Grade;
